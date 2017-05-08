225717274e5ca96abda377c9baf332d617062b9b
Lakshman Kumar <lkumar93@umd.edu>
2017-05-08 03:58:09 -0400

    Update morphological operations and remove histogram equalization

--- mobile_scanner/src/mobile_scanner.cpp
+++ mobile_scanner/src/mobile_scanner.cpp
@@ -32,6 +32,7 @@
 #include "image_enhancement.h"
 #include "edge_detection.h"
 #include "harris_corner_detection.h"
+#include "image_transformation.h"
 
 //#define THRESHOLD_RATIO_H 220
 //#define THRESHOLD_RATIO_L 75
@@ -71,19 +72,19 @@ void order_points(std::vector<IndexT> corner_indices, IndexT ordered_points[])
 		ordered_points[2] = *it;
 	}
 
-	else if(sum < min_sum)
+	if(sum < min_sum)
 	{
 		min_sum = sum ;
-		ordered_points[0] = *it;
+		ordered_points[0] = *it;	
 	}
 
-	if(diff < min_diff )
+	if(diff < min_diff)
 	{
 		min_diff = diff ;
 		ordered_points[1] = *it;
 	}
 
-	else if(diff > max_diff )
+	if(diff > max_diff)
 	{
 		max_diff = diff ;
 		ordered_points[3] = *it;
@@ -98,154 +99,71 @@ void order_points(std::vector<IndexT> corner_indices, IndexT ordered_points[])
 		cout<<"row = "<<ordered_points[i].row<<" , col="<<ordered_points[i].col<<endl;
    }	
 
-}
+  }
 
 
-Mat perspective_transform(Point2f corner_points[], Point2f reference_points[])
+ Mat scan_document(const Mat& input_image, bool image_registration = false)
 {
-	Mat A = Mat::ones( 3, 3, CV_32F );
-	Mat B = Mat::ones( 3, 1, CV_32F );
 
-	A.at<float>(0,0) = corner_points[0].x;
-	A.at<float>(1,0) = corner_points[0].y;
+    Mat grayscale_image, equalized_image,detected_edges, canny_detected_edges,receipt;
 
-	A.at<float>(0,1) = corner_points[1].x;
-	A.at<float>(1,1) = corner_points[1].y;
+    int scale_factor = 4.0;
 
-	A.at<float>(0,2) = corner_points[2].x;
-	A.at<float>(1,2) = corner_points[2].y;
+    Mat rgb_image_resized = Mat::zeros( input_image.rows/scale_factor, input_image.cols/scale_factor, CV_8UC3 );
 
-	B.at<float>(0,0) = corner_points[3].x;
-	B.at<float>(1,0) = corner_points[3].y;
-
-	Mat C = Mat::ones( 3, 1, CV_32F );
-	C = A.inv()*B;
-
-	Mat M = Mat::ones( 3, 3, CV_32F );
-
-	M.at<float>(0,0) = C.at<float>(0,0)*corner_points[0].x;
-	M.at<float>(1,0) =  C.at<float>(0,0)*corner_points[0].y;
-	M.at<float>(2,0) =  C.at<float>(0,0);
-
-	M.at<float>(0,1) =  C.at<float>(1,0)* corner_points[1].x;
-	M.at<float>(1,1) =  C.at<float>(1,0)*corner_points[1].y;
-	M.at<float>(2,1) =  C.at<float>(1,0);
-
-	M.at<float>(0,2) =  C.at<float>(2,0)*corner_points[2].x;
-	M.at<float>(1,2) =  C.at<float>(2,0)*corner_points[2].y;
-	M.at<float>(2,2) =  C.at<float>(2,0);
-
-	Mat D = Mat::ones( 3, 3, CV_32F );
-	Mat E = Mat::ones( 3, 1, CV_32F );
-
-	D.at<float>(0,0) = reference_points[0].x;
-	D.at<float>(1,0) = reference_points[0].y;
-
-	D.at<float>(0,1) = reference_points[1].x;
-	D.at<float>(1,1) = reference_points[1].y;
-
-	D.at<float>(0,2) = reference_points[2].x;
-	D.at<float>(1,2) = reference_points[2].y;
-
-	E.at<float>(0,0) = reference_points[3].x;
-	E.at<float>(1,0) = reference_points[3].y;
-
-	Mat F = Mat::ones( 3, 1, CV_32F );
-	F = D.inv()*E;
-
-	Mat N = Mat::ones( 3, 3	, CV_32F );
-
-	N.at<float>(0,0) = F.at<float>(0,0)*reference_points[0].x;
-	N.at<float>(1,0) = F.at<float>(0,0)*reference_points[0].y;
-	N.at<float>(2,0) = F.at<float>(0,0);
-
-	N.at<float>(0,1) = F.at<float>(1,0)*reference_points[1].x;
-	N.at<float>(1,1) = F.at<float>(1,0)*reference_points[1].y;
-	N.at<float>(2,1) = F.at<float>(1,0);
-
-	N.at<float>(0,2) = F.at<float>(2,0)*reference_points[2].x;
-	N.at<float>(1,2) = F.at<float>(2,0)*reference_points[2].y;
-	N.at<float>(2,2) = F.at<float>(2,0);
-
-
-	Mat perspective_transform= Mat::ones( 3, 3, CV_32F );
-	perspective_transform = N*M.inv();
-
-	return perspective_transform;
-}
-
-///////////////////////////////////////////
-//
-//	MAIN FUNCTION
-//
-///////////////////////////////////////////
-int main(int argc, char** argv )
-{
-    Mat rgb_image, grayscale_image, equalized_image,detected_edges, canny_detected_edges;
-    rgb_image = imread( "../images/receipt.jpg", 1 );
-
-    if ( !rgb_image.data )
-    {
-        printf("No image data \n");
-        return -1;
-    }
-
-    Mat rgb_image_resized = Mat::zeros( 640, 480, CV_8UC3 );
-
-    float row_ratio = 1;//rgb_image.rows/640.0;
-    float col_ratio = 1;//rgb_image.cols/480.0;
-
-    resize(rgb_image, rgb_image_resized , rgb_image_resized.size(), 0, 0);
-   
-    rgb_image = rgb_image_resized;
+    float row_ratio = scale_factor;
+    float col_ratio = scale_factor;
 
    if(USE_OPENCV)
    {
-    cvtColor( rgb_image_resized , grayscale_image, CV_BGR2GRAY );
+	    if(input_image.cols > 480 || input_image.rows >640 )
+	    	resize(input_image, rgb_image_resized , rgb_image_resized.size(), 0, 0);
 
-    equalizeHist(grayscale_image,grayscale_image);
+ 	    else
+		rgb_image_resized = input_image.clone();
 
-    bilateralFilter(grayscale_image, equalized_image, 5, 25, 20);
+	    cvtColor( rgb_image_resized , grayscale_image, CV_BGR2GRAY );
 
-    Canny( equalized_image, canny_detected_edges, 75, 220, 3 );
+	    equalizeHist(grayscale_image,grayscale_image);
+
+	    bilateralFilter(grayscale_image, equalized_image, 5, 25, 20);
+
+	    Canny( equalized_image, canny_detected_edges, 75, 220, 3 );
     }
 
     else
     {
-     grayscale_image =  convert_to_grayscale(rgb_image_resized);
+	     if(input_image.cols > 480 || input_image.rows >640 )
+	     	rgb_image_resized = resize_image(input_image,input_image.cols/scale_factor,input_image.rows/scale_factor);
 
-     equalized_image = equalize_image(grayscale_image);
+ 	     else
+		rgb_image_resized = input_image.clone();
 
-     equalized_image = bilateral_filter(equalized_image, 5, 15, 10);
+	     grayscale_image =  convert_to_grayscale(rgb_image_resized);
 
-     if(equalized_image.type() != CV_8UC1)
-	equalized_image.convertTo(equalized_image, CV_8UC1);
+	     grayscale_image =	gaussian_filter(grayscale_image,3, 1.4);
 
-     equalized_image = sharpen(equalized_image,5, 0.1);
+	     equalized_image = grayscale_image; // equalize_image(grayscale_image);
 
-     canny_detected_edges = canny_edge_detection(equalized_image, THRESHOLD_RATIO_L, THRESHOLD_RATIO_H);
+	     equalized_image = bilateral_filter(equalized_image, 5, 25, 15);
 
+	     if(equalized_image.type() != CV_8UC1)
+		equalized_image.convertTo(equalized_image, CV_8UC1);
+
+	     equalized_image = sharpen(equalized_image,5, 0.1);
+
+	     canny_detected_edges = canny_edge_detection(equalized_image, THRESHOLD_RATIO_L, THRESHOLD_RATIO_H);
     }
+   
+    detected_edges = canny_detected_edges;
 
-    detected_edges =  morphological_filter( canny_detected_edges, 5, true, false);
-  
-    if(detected_edges.type() != CV_8UC1)
-	    detected_edges.convertTo(detected_edges, CV_8UC1);
-
+    detected_edges =  morphological_filter( detected_edges, 5, true, false);
     detected_edges =  morphological_filter( detected_edges, 3, false, false);
-
-    if(detected_edges.type() != CV_8UC1)
-	    detected_edges.convertTo(detected_edges, CV_8UC1);
-
+    //detected_edges = median_filter(detected_edges,3);
     detected_edges =  morphological_filter( detected_edges, 3, false, false);
-
-    if(detected_edges.type() != CV_8UC1)
-	    detected_edges.convertTo(detected_edges, CV_8UC1);
-
-    detected_edges =  morphological_filter( detected_edges, 3, true, false);
-
-    if(detected_edges.type() != CV_8UC1)
-	    detected_edges.convertTo(detected_edges, CV_8UC1);
+    detected_edges =  morphological_filter( detected_edges, 7, true, false);
+    detected_edges =  morphological_filter( detected_edges, 5, false, false);
+    detected_edges =  morphological_filter( detected_edges, 3, false, false);
 
     vector<vector<Point> > contours;
     vector<Vec4i> hierarchy;
@@ -268,172 +186,178 @@ int main(int argc, char** argv )
 	  int largest_contour_center_y;
 	  int number_of_vertices;          
 
-	  Mat image_of_contours =  Mat::zeros( 640, 480, CV_8UC1 );//rgb_image_resized;//Mat::zeros( detected_edges.size(), CV_8UC3 );;
+	  Mat image_of_contours =  Mat::zeros( input_image.rows/scale_factor, input_image.cols/scale_factor, CV_8UC1 );//rgb_image_resized;//Mat::zeros( detected_edges.size(), CV_8UC3 );;
+	  Mat viz_corner = rgb_image_resized.clone();
+	  Mat viz_contour =  rgb_image_resized.clone();
 
  	  RNG rng(12345);
 
 	  // Find contour with largest area
 	  for( int i = 0; i < number_of_contours ; i++ )
-	     { 
-
+	  { 
       		cv::approxPolyDP(cv::Mat(contours[i]), contours[i], cv::arcLength(cv::Mat(contours[i]), true)*0.05, true);
 		current_area = contourArea(contours[i]);
 		current_number_of_vertices = contours[i].size();
 	
-		if ( current_number_of_vertices == 4 && current_area > previous_area )
+		if ( current_area > previous_area )
 		{
-			//
 			largest_contour_area = current_area;
 			largest_contour_id = i;
 			number_of_vertices = current_number_of_vertices;	
 			previous_area = current_area;	
 		}
-	   // Draw contours
-//       	   Scalar color = Scalar( 255);//, 255, 0 );
-//       	   drawContours( image_of_contours , contours, i, color, 2, 8, hierarchy, 0, Point() );
 
-	     }
+	  }
 
-       	   Scalar color = Scalar( 255);//, 255, 0 );
-       	   drawContours( image_of_contours , contours, largest_contour_id, color, 2, 8, hierarchy, 0, Point());	
+       	 Scalar color = Scalar( 255);
+       	 drawContours( image_of_contours , contours, largest_contour_id, color, 2, 8, hierarchy, 0, Point());
+      	 drawContours( viz_contour , contours, largest_contour_id, Scalar(0, 255,0),2, 8, hierarchy, 0, Point());	
 
 	 Mat detected_corners;
-//	  /// Detecting corners
-//	  cornerHarris( image_of_contours, detected_corners, 2, 3, 0.04, BORDER_DEFAULT );
 
-//	  /// Normalizing
-//	  normalize(  detected_corners,  detected_corners, 0, 255, NORM_MINMAX, CV_32FC1, Mat() );
-//	  convertScaleAbs(  detected_corners,  detected_corners );
+	 CornerT corner_data = harris_corner_detection(image_of_contours,-0.25,10,0.03,4);
 
+	 detected_corners = corner_data.corner_image;
 
-////	  /// Drawing a circle around corners
+	 IndexT ordered_points[4];
 
-	  CornerT corner_data = harris_corner_detection(image_of_contours);
+	 if(corner_data.corner_indices.size() < 4)
+	 {
+		cout<<"Cannot process image with less than 4 corners"<<endl;
+		return input_image;
+  	 }
+	 else
+	 {
+		cout<<"Image with "<<corner_data.corner_indices.size()<<" corners found"<<endl;
+	  }
 
-//    waitKey(0);
+	 order_points(corner_data.corner_indices, ordered_points);
 
-//    return 0;
-	  detected_corners = corner_data.corner_image;
+	 for(int i = 0 ; i<4 ; i++)
+	 {
+		cout<<"row = "<<ordered_points[i].row*row_ratio<<" , col="<<ordered_points[i].col*col_ratio<<endl;
+		circle( viz_corner, Point( ordered_points[i].col, ordered_points[i].row ), 10,  Scalar(0,255,0), 2, 8, 0 );
+		ordered_points[i].row = int(ordered_points[i].row*row_ratio);
+		ordered_points[i].col = int(ordered_points[i].col*col_ratio);
+	 }	
 
-	  IndexT ordered_points[4];
+	 int width1 = int(sqrt(pow(ordered_points[3].col - ordered_points[2].col,2) + pow(ordered_points[3].row - ordered_points[2].row,2)));
+	 int width2 = int(sqrt(pow(ordered_points[1].col - ordered_points[0].col,2) + pow(ordered_points[1].row - ordered_points[0].row,2)));
 
-	  order_points(corner_data.corner_indices, ordered_points);
+	 int width = (width1 > width2) ? width1 : width2;
 
-	   for(int i = 0 ; i<4 ; i++)
-	   {
-			cout<<"row = "<<ordered_points[i].row<<" , col="<<ordered_points[i].col<<endl;
-			ordered_points[i].row = int(ordered_points[i].row*row_ratio);
-			ordered_points[i].col = int(ordered_points[i].col*col_ratio);
-	   }	
+	 int height1 = int(sqrt(pow(ordered_points[1].col - ordered_points[2].col,2) + pow(ordered_points[1].row - ordered_points[2].row,2)));
+	 int height2 = int(sqrt(pow(ordered_points[3].col - ordered_points[0].col,2) + pow(ordered_points[3].row - ordered_points[0].row,2)));
 
-	   int width1 = int(sqrt(pow(ordered_points[3].col - ordered_points[2].col,2) + pow(ordered_points[3].row - ordered_points[2].row,2)));
-	   int width2 = int(sqrt(pow(ordered_points[1].col - ordered_points[0].col,2) + pow(ordered_points[1].row - ordered_points[0].row,2)));
+	 int height = (height1 > height2) ? height1 : height2;
 
-	   int width = (width1 > width2) ? width1 : width2;
+	 Point2f input_rectangle[4], output_rectangle[4];
 
-	   int height1 = int(sqrt(pow(ordered_points[1].col - ordered_points[2].col,2) + pow(ordered_points[1].row - ordered_points[2].row,2)));
-	   int height2 = int(sqrt(pow(ordered_points[3].col - ordered_points[0].col,2) + pow(ordered_points[3].row - ordered_points[0].row,2)));
+	 input_rectangle[0] = Point2f(ordered_points[0].col, ordered_points[0].row);
+	 input_rectangle[1] = Point2f(ordered_points[1].col, ordered_points[1].row);
+	 input_rectangle[2] = Point2f(ordered_points[2].col, ordered_points[2].row);
+	 input_rectangle[3] = Point2f(ordered_points[3].col, ordered_points[3].row);
 
-	   int height = (height1 > height2) ? height1 : height2;
+	 output_rectangle[0] = Point2f(0,0);
+	 output_rectangle[1] = Point2f(width-1,0);
+	 output_rectangle[2] = Point2f(width-1,height-1);
+	 output_rectangle[3] = Point2f(0,height-1);
 
-	   Point2f input_rectangle[4], output_rectangle[4];
+         Mat lambda( 2, 4, CV_32FC1 );
 
-	   input_rectangle[0] = Point2f(ordered_points[0].col, ordered_points[0].row);
-	   input_rectangle[1] = Point2f(ordered_points[1].col, ordered_points[1].row);
-	   input_rectangle[2] = Point2f(ordered_points[2].col, ordered_points[2].row);
-	   input_rectangle[3] = Point2f(ordered_points[3].col, ordered_points[3].row);
+	 int vacant_pixel_value = 0;
 
-	   output_rectangle[0] = Point2f(0,0);
-	   output_rectangle[1] = Point2f(width-1,0);
-	   output_rectangle[2] = Point2f(width-1,height-1);
-	   output_rectangle[3] = Point2f(0,height-1);
+	 Mat warped_image = Mat::ones(height,width,CV_8UC1)*vacant_pixel_value;
 
-           Mat lambda( 2, 4, CV_32FC1 );
+	rgb_image_resized = input_image.clone();
 
-	   Mat warped_image = Mat::ones(height,width,CV_8UC1)*0;
-
-
-	  if(USE_OPENCV)   	
-	  {
-	 
+	 if(USE_OPENCV)   	
+	 {	 
 		  lambda = getPerspectiveTransform( input_rectangle, output_rectangle );
 
-		  warpPerspective(rgb_image,warped_image,lambda,warped_image.size() );
+		  warpPerspective(rgb_image_resized,warped_image,lambda,warped_image.size() );
 
 		  cvtColor(warped_image,warped_image, CV_BGR2GRAY);
-
 	  }
 
 	  else
 	  {
 
-	   Mat pt =  perspective_transform(input_rectangle, output_rectangle);
+		   Mat pt =  perspective_transform(input_rectangle, output_rectangle);
 
-	   Mat transformed_point = Mat::ones(3,1,CV_32F);
-	   Mat given_point = Mat::ones(3,1,CV_32F); 
+		   warp_image(convert_to_grayscale(rgb_image_resized), warped_image, pt);
 
-	   for( int j = 0; j <  grayscale_image.rows ; j++ )
-   	      for( int i = 0; i <  grayscale_image.cols; i++ )
-		{
-			given_point.at<float>(0,0) = i;
-			given_point.at<float>(1,0) = j;
+		   bilinearInterpolation(warped_image,vacant_pixel_value);
 
-			transformed_point = pt*given_point;
-
-			int x = cvFloor(transformed_point.at<float>(1,0)/transformed_point.at<float>(2,0));
-			int y = cvFloor(transformed_point.at<float>(0,0)/transformed_point.at<float>(2,0));
-
-			if(x >= 0 && x < height && y >=0 && y< width)
-				warped_image.at<uchar>(x,y) = grayscale_image.at<uchar>(j,i);
-
-		}
-
-	   warped_image = median_filter(warped_image, 3);
-
-	   if(warped_image.type()!=CV_8UC1)
-		warped_image.convertTo(warped_image, CV_8UC1);
+		   if(warped_image.type()!=CV_8UC1)
+			warped_image.convertTo(warped_image, CV_8UC1);
 	   }
 
-	  Mat receipt = warped_image;
+	  receipt = warped_image;
 
 	  threshold_image(warped_image, receipt, 6, false, true);
 
-	  namedWindow("Input", WINDOW_AUTOSIZE);
-          imwrite( "../results/mobile_scanner/resized_input.jpg", rgb_image_resized );
-	  imshow("Input",rgb_image_resized);
+	  if(!image_registration)
+	  {
+		  namedWindow("Input", WINDOW_AUTOSIZE);
+		  imwrite( "../results/mobile_scanner/resized_input.jpg", rgb_image_resized );
+		  imshow("Input",rgb_image_resized);
 
-	  namedWindow("Image Enhancement", WINDOW_AUTOSIZE);
-           imwrite( "../results/mobile_scanner/image_enhancement.jpg", equalized_image );
-	  imshow("Image Enhancement",equalized_image);  
+		  namedWindow("Image Enhancement", WINDOW_AUTOSIZE);
+		   imwrite( "../results/mobile_scanner/image_enhancement.jpg", equalized_image );
+		  imshow("Image Enhancement",equalized_image);  
 
-	  namedWindow("Canny Edge Detection", WINDOW_AUTOSIZE);
-          imwrite( "../results/mobile_scanner/canny_edge_detection.jpg", canny_detected_edges );
-	  imshow("Canny Edge Detection",canny_detected_edges);  
+		  namedWindow("Canny Edge Detection", WINDOW_AUTOSIZE);
+		  imwrite( "../results/mobile_scanner/canny_edge_detection.jpg", canny_detected_edges );
+		  imshow("Canny Edge Detection",canny_detected_edges);  
 
-	  namedWindow("Morphological Operations", WINDOW_AUTOSIZE);
-          imwrite( "../results/mobile_scanner/morphological_operation.jpg", detected_edges );
-	  imshow("Morphological Operations",detected_edges);  
+		  namedWindow("Morphological Operations", WINDOW_AUTOSIZE);
+		  imwrite( "../results/mobile_scanner/morphological_operation.jpg", detected_edges );
+		  imshow("Morphological Operations",detected_edges);  
 
-	  namedWindow("Contour Detection", WINDOW_AUTOSIZE);
-          imwrite( "../results/mobile_scanner/contour_detection.jpg", image_of_contours );
-	  imshow("Contour Detection",image_of_contours);
+		  namedWindow("Contour Detection", WINDOW_AUTOSIZE);
+		  imwrite( "../results/mobile_scanner/contour_detection.jpg", viz_contour );
+		  imshow("Contour Detection",viz_contour);
 
-	  namedWindow("Corner Detection", WINDOW_AUTOSIZE);
-          imwrite( "../results/mobile_scanner/corner_detection.jpg", detected_corners );
-	  imshow("Corner Detection",detected_corners);
+		  namedWindow("Corner Detection", WINDOW_AUTOSIZE);
+		  imwrite( "../results/mobile_scanner/corner_detection.jpg", detected_corners );
+		  imshow("Corner Detection",detected_corners);
 
-	  namedWindow("Warp with Perspective Transform", WINDOW_AUTOSIZE);
-          imwrite( "../results/mobile_scanner/warped_image.jpg", warped_image );
-	  imshow("Warp with Perspective Transform",warped_image);	  
+		  namedWindow("Warp with Perspective Transform", WINDOW_AUTOSIZE);
+		  imwrite( "../results/mobile_scanner/warped_image.jpg", warped_image );
+		  imshow("Warp with Perspective Transform",warped_image);	  
 
-	  namedWindow("Adaptive Thresholding", WINDOW_AUTOSIZE);
-          imwrite( "../results/mobile_scanner/adaptive_thresholding.jpg", receipt );
-	  imshow("Adaptive Thresholding",receipt);
+		  namedWindow("Adaptive Thresholding", WINDOW_AUTOSIZE);
+		  imwrite( "../results/mobile_scanner/adaptive_thresholding.jpg", receipt );
+		  imshow("Adaptive Thresholding",receipt);
+	  }
 
 	  cout<<"Width = "<<width<<std::endl;
 	  cout<<"Height = "<<height<<std::endl;
 
    }
+
+
+   return receipt;
+}
+
+///////////////////////////////////////////
+//
+//	MAIN FUNCTION
+//
+///////////////////////////////////////////
+int main(int argc, char** argv )
+{
+    Mat rgb_image, grayscale_image, equalized_image,detected_edges, canny_detected_edges;
+    rgb_image = imread( "../images/receipt.jpg", 1 );
+
+    if ( !rgb_image.data )
+    {
+        printf("No image data \n");
+        return -1;
+    }
+
+    Mat receipt1 = scan_document(rgb_image);
     
     waitKey(0);
 
